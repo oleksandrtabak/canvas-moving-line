9827a404ae13a2ac485d12d8ff372e548af01e0c
Олександр <oleksandrtabak@protonmail.com>
2022-07-16 17:55:22 +0300

    Створено лінії

--- move.js
+++ move.js
@@ -7,13 +7,21 @@ const HEIGHT = 250
 canvas.width = WIDTH
 canvas.height = WIDTH
 
-ctx.fillStyle = 'rgb(180, 230, 250, 0)'
-ctx.fillRect(0, 0, WIDTH / 2, HEIGHT / 2)
 
+const gradient = ctx.createLinearGradient(50, 50, WIDTH - 50, HEIGHT - 50)
+gradient.addColorStop(0, '#87bfff')
+gradient.addColorStop(1, '#cdf0ff')
 
-ctx.fillStyle = 'rgb(150, 200, 250)'
-ctx.beginPath()
-ctx.moveTo(125, 125)
-ctx.lineTo(125, 50)
-ctx.lineTo(200, 50)
-ctx.fill()
+ctx.fillStyle = gradient
+ctx.fillRect(0, 0, WIDTH, HEIGHT)
+
+ctx.lineWidth = 1
+ctx.lineCap = "round"
+
+for (let i = 0; i < 7; i++) {
+  ctx.lineWidth += 1
+  ctx.beginPath()
+  ctx.moveTo(50 + i * 20 + ctx.lineWidth, 50)
+  ctx.lineTo(50 + i * 20 + ctx.lineWidth, 200)
+  ctx.stroke()
+}
