fad255bc707663d3d2e10e4315cee30b49f1b84b
Олександр <oleksandrtabak@protonmail.com>
2022-07-16 16:56:06 +0300

    Створено трикутник

--- move.js
+++ move.js
@@ -1,5 +1,19 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-ctx.fillStyle = 'lightgreen'
-ctx.fillRect(10, 10, 150, 100)
+const WIDTH = 250
+const HEIGHT = 250
+
+canvas.width = WIDTH
+canvas.height = WIDTH
+
+ctx.fillStyle = 'rgb(180, 230, 250, 0)'
+ctx.fillRect(0, 0, WIDTH / 2, HEIGHT / 2)
+
+
+ctx.fillStyle = 'rgb(150, 200, 250)'
+ctx.beginPath()
+ctx.moveTo(125, 125)
+ctx.lineTo(125, 50)
+ctx.lineTo(200, 50)
+ctx.fill()
